19cc02a7d9d82a0633dabd311b70dbfe11e2902b
jpickhardt <jpickhardt@vmware.com>
2020-05-12 10:22:46 -0600

    Add required-version to Terraform block for sprint2 demo AWS TF config.

--- demos/sprint2/sprint2-aws.tf
+++ demos/sprint2/sprint2-aws.tf
@@ -1,6 +1,10 @@
 variable "aws_access_key" {}
 variable "aws_secret_key" {}
 
+terraform {
+  required_version = ">= 11.0"
+}
+
 provider "aws" {
   access_key = var.access_key
   secret_key = var.secret_key
